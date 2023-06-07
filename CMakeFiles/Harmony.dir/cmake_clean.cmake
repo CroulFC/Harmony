684a27fab7267700c42932c70db56f0a8c8a98c4
croul <croulfc@gmail.com>
2023-06-07 21:46:52 +0500

    Initial commit

---
+++ CMakeFiles/Harmony.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Build/Harmony"
+  "Build/Harmony.pdb"
+  "CMakeFiles/Harmony.dir/Source/Main.cpp.o"
+  "CMakeFiles/Harmony.dir/Source/Main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Harmony.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
