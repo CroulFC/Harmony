bfdd63cce8d5d17f423af8df4361bc7b8ca99a24
croul <croulfc@gmail.com>
2023-06-12 21:35:05 +0500

    Реализован основной функционал класса WavFile

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,85 +1,62 @@
-#include <iostream>
-#include <fstream>
-
 #include "WavFile.h"
 
-class WavFile{
-	private:
-	WAV_FILE_HEADER* _file_header;
-	std::ofstream _wav_file;
+#include <iostream>
+#include <cmath>
 
-	static constexpr char* _default_file_name = (char* const)"result.wav";
-	static constexpr uint32_t _default_sample_rate = (uint32_t)48000;
-	static constexpr uint16_t _default_num_of_channels = (uint16_t)1;
-	static constexpr uint16_t _default_byte_per_sample = (uint16_t)1;
+void doTest(const std::string &file_path)
+{
+	//Try to create WAV-file with 10 secs of 1 KHz sin
+	static constexpr uint32_t TEST_SOUND_LENGTH = 5u;
+	static constexpr uint32_t TEST_SOUND_FREQUENCY = 300u;
+	static constexpr uint32_t TEST_SAMPLE_RATE = 48000u;
+	static constexpr uint32_t TEST_AMPLITUDE = 100u;
+	static constexpr uint8_t TEST_BYTES_PER_SAMPLE = 1;
 
-	void FillFileHeader(
-		uint32_t sample_rate = _default_sample_rate,
-		uint16_t num_of_ch = _default_num_of_channels,
-		uint16_t bytes_per_sample = _default_byte_per_sample
-	)
-	{
-		_file_header->riff_sign[0] = 'R';
-		_file_header->riff_sign[1] = 'I';
-		_file_header->riff_sign[2] = 'F';
-		_file_header->riff_sign[3] = 'F';
+	WavFile test_wav_file(TEST_SAMPLE_RATE, TEST_BYTES_PER_SAMPLE);
 
-		_file_header->wave_sign[0] = 'W';
-		_file_header->wave_sign[1] = 'A';
-		_file_header->wave_sign[2] = 'V';
-		_file_header->wave_sign[3] = 'E';
+	test_wav_file.Initialize(file_path, WavFile::OPEN_MODE::WRITE);
 
-		_file_header->fmt_sign[0] = 'f';
-		_file_header->fmt_sign[1] = 'm';
-		_file_header->fmt_sign[2] = 't';
-		_file_header->fmt_sign[3] = ' ';
+	uint8_t *one_second = new uint8_t[TEST_SAMPLE_RATE];
 
-		_file_header->data_sign[0] = 'd';
-		_file_header->data_sign[1] = 'a';
-		_file_header->data_sign[2] = 't';
-		_file_header->data_sign[3] = 'a';
-
-		_file_header->fmt_chunk_size = FMT_CHUNK_SIZE;
-		_file_header->audio_format = WAV_AUDIO_FORMAT_PCM;
-		_file_header->num_of_channels = num_of_ch;
-		_file_header->sample_rate = sample_rate;
-		_file_header->byte_rate = sample_rate * bytes_per_sample;
-		_file_header->sample_aligment = bytes_per_sample * num_of_ch;
-		_file_header->bit_depth = bytes_per_sample * 8;
-
-		return;
-	}
-
-	void FillHeaderPostInfo(size_t data_size){
-		_file_header->wav_size = data_size + sizeof(WAV_FILE_HEADER) 
-		- (sizeof(char)*4 + sizeof(uint32_t)); //Size of RIFF + size of wav_size field
-
-		_file_header->data_bytes = data_size;
-
-		return;
-	}
-
-	public:
-	WavFile(std::string file_name = _default_file_name,
-	uint32_t sample_rate = _default_sample_rate)
-	{
-		_file_header = new WAV_FILE_HEADER;
-		if(!_file_header){
-			throw "Can't allocate memory for WAV header\n";
-		}
-		try{
-			_wav_file = std::ofstream(file_name);
-		}
-		catch(std::exception& e){
-			throw e;
+	for(uint32_t s = 0; s < TEST_SOUND_LENGTH; ++s){
+		for(uint32_t i = 0; i < TEST_SAMPLE_RATE; ++i){
+			double argument = TEST_SOUND_FREQUENCY * i * 2 * M_PI / TEST_SAMPLE_RATE;
+			one_second[i] = TEST_AMPLITUDE * sin(argument);
 		}
 
-		return;
+		test_wav_file.Write(one_second, TEST_SAMPLE_RATE);
 	}
 
-};
+	test_wav_file.Complete();
+
+	delete[] one_second;
+}
+
+static void showUsage(const std::string &app_path)
+{
+	std::cout << "Usage: " << app_path << " <wav file path>" << std::endl;
+}
 
 int main(int argc, char* argv[]){
-	std::cout << "Hello!" << std::endl;
-	return 0;
+
+	// Check args
+	if (argc < 2)
+	{
+		std::cerr << "Invalid args" << std::endl;
+		showUsage(argv[0]);
+		return EXIT_SUCCESS;
+	}
+
+	// Run tests
+	try
+	{
+		doTest(argv[1]);
+	}
+	catch (const std::exception &ex)
+	{
+		std::cerr << "Test failure: " << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
--- Source/WavFile.h
+++ Source/WavFile.h
@@ -1,27 +1,59 @@
-#pragma once
-#include <stdint.h>
+#ifndef SOURCE_WAVFILE_HPP_
+#define SOURCE_WAVFILE_HPP_
 
-static constexpr uint8_t   WAV_FILE_HEADER_SIZE = 44;
-static constexpr uint32_t  FMT_CHUNK_SIZE = 16; 
+#include <string>
+#include <fstream>
 
-static constexpr uint16_t  WAV_AUDIO_FORMAT_PCM = 1; 
+class WavFile{
+	private:
 
-typedef struct _WAV_FILE_HEADER{
-    char riff_sign[4];
-    uint32_t wav_size;
+	struct WAV_FILE_HEADER{
+		char riff_sign[4];
+		uint32_t wav_size;
+		char wave_sign[4];
+		char fmt_sign[4];
+		uint32_t fmt_chunk_size;
+		uint16_t audio_format;
+		uint16_t num_of_channels;
+		uint32_t sample_rate;
+		uint32_t byte_rate;
+		uint16_t sample_aligment;
+		uint16_t bit_depth;
+		char data_sign[4];
+		uint32_t data_bytes;
+	} m_file_header{};
 
-    char wave_sign[4];
-    char fmt_sign[4];
-    uint32_t fmt_chunk_size;
-    uint16_t audio_format;
-    uint16_t num_of_channels;
-    uint32_t sample_rate;
-    uint32_t byte_rate;
-    uint16_t sample_aligment;
-    uint16_t bit_depth;
+	std::fstream m_wav_file{};
 
-    char data_sign[4];
-    uint32_t data_bytes;
-} WAV_FILE_HEADER;
+	size_t m_total_data_size = 0;
 
-static_assert(sizeof(WAV_FILE_HEADER) == WAV_FILE_HEADER_SIZE,"Invalid WAV_FILE_HEADER structure size");
+	void FillFileHeader(uint32_t sample_rate, uint16_t bytes_per_sample) noexcept;
+	void CompleteFile();
+
+	public:
+	enum class OPEN_MODE{
+		READ = 0,
+		WRITE,
+		NOT_OPEN
+	};
+
+	explicit WavFile(uint32_t sample_rate, uint8_t bytes_per_sample);
+
+	explicit WavFile(const WavFile&) = delete;
+	WavFile& operator=(const WavFile&) = delete;
+
+	WavFile(WavFile &&wavFile) noexcept;
+	WavFile& operator=(WavFile &&wavFile) noexcept;
+
+	virtual ~WavFile();
+	void Initialize(const std::string& file_name, OPEN_MODE open_mode);
+	void Write(const uint8_t* data, size_t data_size);
+	void Read(uint8_t* buffer, size_t data_size);
+	void Complete();
+	OPEN_MODE GetOpenMode() const noexcept;
+
+	private:
+	OPEN_MODE m_file_open_mode{};
+};
+
+#endif // !SOURCE_WAVFILE_HPP_
