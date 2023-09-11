688c0942703c1d2f66a941c8f17e2a9a30aa5995
croul <croulfc@gmail.com>
2023-09-11 09:20:58 +0200

    Класс Parameter инкапсулирован в класс ArgumentsParser, удалены конструкторы без параметров, добавлена опция для значения параметра NOT_REQUIRED

--- Source/ArgsParser.h
+++ Source/ArgsParser.h
@@ -2,28 +2,6 @@
 #include <map>
 #include <any>
 
-class Parameter final{
-	public:
-	enum class PARAM_PRESENCE {
-		OPTIONAL = 0,
-		REQUIRED
-	};
-
-	enum class VALUE_PRESENCE{
-		OPTIONAL = 0,
-		REQUIRED
-	};
-
-	private:
-	char 			m_short_name = '\0';
-	std::string 	m_full_name{};
-	PARAM_PRESENCE	m_param_presence = PARAM_PRESENCE::OPTIONAL;
-	VALUE_PRESENCE	m_value_presence = VALUE_PRESENCE::OPTIONAL;
-
-	public:
-	Parameter(char short_name, std::string full_name, PARAM_PRESENCE parameter_presence, VALUE_PRESENCE value_presence);
-};
-
 class ArgumentsParser final{
 	public:
 	enum class PARSE_STATUS{
@@ -32,14 +10,41 @@ class ArgumentsParser final{
 		NOT_PARSED
 	};
 
-	private:
+	class Parameter final{
+		public:
+		enum class PARAM_PRESENCE {
+			OPTIONAL = 0,
+			REQUIRED
+		};
 
-	std::map<std::string, std::any> m_arguments{};
-	PARSE_STATUS					m_parse_status = PARSE_STATUS::NOT_PARSED;
+		enum class VALUE_PRESENCE{
+			NOT_REQUIRED = 0,
+			REQUIRED,
+			OPTIONAL
+		};
+
+		const char 			EMPTY_SHORT_NAME = '\0';
+		const std::string 	EMPTY_FULL_NAME = std::string("");
+
+		private:
+		char 			m_short_name = '\0';
+		std::string 	m_full_name{};
+		PARAM_PRESENCE	m_param_presence = PARAM_PRESENCE::OPTIONAL;
+		VALUE_PRESENCE	m_value_presence = VALUE_PRESENCE::OPTIONAL;
+
+		public:
+		Parameter() = delete;
+		Parameter(char short_name, std::string full_name, PARAM_PRESENCE parameter_presence, VALUE_PRESENCE value_presence);
+	};
+
+	private:
+	std::map<const Parameter&, char*> 	m_arguments{};
+	PARSE_STATUS						m_parse_status = PARSE_STATUS::NOT_PARSED;
 
 	public:
-	explicit ArgumentsParser(uint32_t argc, char** argv);
-	void AddParam(Parameter& const param);
-	void Parse();
-	std::any GetValueOfParameter(std::string& const param_name);
+	ArgumentsParser() = delete;
+	explicit ArgumentsParser(int argc, char** argv);
+	void AddParam(const Parameter& param);
+	PARSE_STATUS Parse();
+	std::any GetValueOfParameter(const std::string& param_name);
 };
